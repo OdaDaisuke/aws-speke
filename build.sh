ebbcd7b7200f5bfec70df78c45a8fba1151c3399
OdaDaisuke <hinopon25@gmail.com>
2019-12-20 22:03:16 +0900

    Add build cmd

---
+++ build.sh
@@ -0,0 +1,7 @@
+ORIGIN=`pwd`
+cp -rc ./src ./tmp
+docker run --rm -v "$PWD/tmp":/var/task lambci/lambda:build-python3.6 pip install -r requirements.txt -t .
+cd tmp
+zip -r dist *
+cd $ORIGIN
+mv ./tmp/dist.zip ./dist.zip
