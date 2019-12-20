dcfbabcb6266d3ed98127fd72470b2402f91a606
OdaDaisuke <hinopon25@gmail.com>
2019-12-20 22:07:50 +0900

    Fix permission

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm dist.zip
 ORIGIN=`pwd`
 cp -rc ./src ./tmp
 docker run --rm -v "$PWD/tmp":/var/task lambci/lambda:build-python3.6 pip install -r requirements.txt -t .
@@ -5,3 +6,4 @@ cd tmp
 zip -r dist *
 cd $ORIGIN
 mv ./tmp/dist.zip ./dist.zip
+rm -rf tmp
